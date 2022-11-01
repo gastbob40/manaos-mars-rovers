9b74dcb77bea8c73b08fb051067c000b00b88b48
Quentin Briolant <quentin.briolant@epita.fr>
2022-11-01 20:52:16 +0100

    docs(api): improve swagger
    Signed-off-by: Quentin Briolant <quentin.briolant@epita.fr>

--- src/main/java/com/gastbob40/presentation/rest/RoverResource.java
+++ src/main/java/com/gastbob40/presentation/rest/RoverResource.java
@@ -1,6 +1,9 @@
 package com.gastbob40.presentation.rest;
 
 import com.gastbob40.domain.service.RoverService;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -12,10 +15,20 @@ import javax.ws.rs.core.MediaType;
 @Path("/rovers")
 @Consumes(MediaType.TEXT_PLAIN)
 @Produces(MediaType.TEXT_PLAIN)
+
 public class RoverResource {
     @Inject RoverService roverService;
 
     @POST @Path("/simulate")
+    @Operation(
+            summary = "Simulate a rover movement based on input commands",
+            description = "Apply commands on multiple rovers to simulate their movements",
+            operationId = "rover-simulate"
+    )
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Rover simulation successful"),
+            @APIResponse(responseCode = "400", description = "Invalid input"),
+    })
     public String simulateRovers(String request) {
         return roverService.simulate(request);
     }
